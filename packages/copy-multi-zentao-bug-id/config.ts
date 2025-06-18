0227d02dc7f96935d17f4ca10921b836715a9047
Gorvey <gorvey76@outlook.com>
2025-06-18 15:56:09 +0800

    feat: update config

--- packages/copy-multi-zentao-bug-id/config.ts
+++ packages/copy-multi-zentao-bug-id/config.ts
@@ -2,6 +2,7 @@ import type { MonkeyOption } from 'vite-plugin-monkey'
 const config: MonkeyOption = {
   entry: './packages/copy-multi-zentao-bug-id/index.ts',
   userscript: {
+    version: '0.0.1',
     'run-at': 'document-idle' as const,
     require: 'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4',
     match: ['http://pms.seevin.com/company-dynamic-account-*.html', 'http://pms.seevin.com/my/'],
@@ -10,7 +11,7 @@ const config: MonkeyOption = {
     name: 'copy-multi-zentao-bug-id',
   },
   build: {
-    fileName: 'codesign-css-gen.user.js',
+    fileName: 'copy-multi-zentao-bug-id.js',
   },
 }
 export default config
